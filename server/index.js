ab4b98686fc0a5a680d0ab8bd68ac148aeb78a23
Gary-Huynh <gary.c.huynh@gmail.com>
2023-08-21 17:03:15 +0100

    fixed server cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ let chatRoom = ""
 let allUsers = []
 const io = new Server(server, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: '*',
       methods: ['GET', 'POST'],
     },
   })
